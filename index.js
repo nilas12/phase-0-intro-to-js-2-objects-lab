082dea6c8d04e559b90c7aaeb8c8ac47c6a1a53a
Your Name <senasezer@sena-MacBook-Air.local>
2022-10-21 13:44:49 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+const employee = {}
+ function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return Object.assign({}, employee, { [key]: value });
+}
+ function  deleteFromEmployeeByKey(employee, key) {
+    const newObj = Object.assign({}, employee);
+    delete newObj[key];
+    return newObj;
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+   Object.assign({}, employee);
+    delete employee[key];
+    return employee;
+  }
